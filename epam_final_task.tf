a199eaf0b78ef7a8d948a77b7270caf8a41e7309
Eduard <44161942+SeverSet@users.noreply.github.com>
2022-07-06 19:35:30 +0300

    Update tf file

--- epam_final_task.tf
+++ epam_final_task.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "server" {
 }
 
 data "aws_eip" "my_instance_eip" {
-  public_ip = "35.157.89.13"
+  public_ip = "3.74.197.60"
 }
 
 resource "aws_eip_association" "eip_assoc" {
@@ -19,34 +19,27 @@ resource "aws_eip_association" "eip_assoc" {
 }
 
 resource "aws_security_group" "server_SG" {
+  dynamic "ingress" {
+    for_each = ["22", "80", "443", "3306", "8080"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
   egress {
-    cidr_blocks = ["0.0.0.0/0", ]
-    description = ""
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0", ]
-    description = ""
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0", ]
-    description = ""
-    from_port   = 8080
-    protocol    = "tcp"
-    to_port     = 8080
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0", ]
-  }
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
 }
 
